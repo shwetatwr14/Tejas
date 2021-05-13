460520690925ae514eb4e6cae53df32fb54d7355
shweta97vish <shweta97vish@gmail.com>
2021-05-14 00:03:50 +0530

    Adding Symbols to Header

--- my-app/src/MyComponents/UpperHeader.js
+++ my-app/src/MyComponents/UpperHeader.js
@@ -1,29 +1,26 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
-import { faCoffee } from '@fontawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faPhoneAlt} from '@fortawesome/free-solid-svg-icons'
 export default function UpperHeader() {
     const mystyle = {
         backgroundColor: "#fd7e14",
         height: "45px"
-      };
+    };
     return (
         <div>
             <Navbar style={mystyle}>
-            <FontAwesomeIcon icon={faCoffee} />
 
-    <Navbar.Brand href="#home">
-      <img
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
-    </Navbar.Brand>
-  </Navbar>
+                <Navbar.Brand href="#home">
+                    <div>                    
+                        <FontAwesomeIcon icon={faEnvelope} size="xs"/>
+                        <FontAwesomeIcon icon={faPhoneAlt} size="xs"/>
+                    </div>
+                </Navbar.Brand>
+            </Navbar>
         </div>
     )
 }
